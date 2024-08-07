2eb8fade358be8f98029794ec3de36ded2e4a670
ArsalanKhan04 <arsal4an@gmail.com>
2024-08-07 13:50:15 +0500

    Made correct changes for 1 Node LSTM

--- models/wavenet/alternate.py
+++ models/wavenet/alternate.py
@@ -4,13 +4,12 @@ from torch.nn import functional as F
 
 class AlternateLayer(nn.Module):
     def __init__(self, input_shape):
-        self.batch_size, self.seq_len, self.input_size = input_shape
+        self.seq_len, self.input_size = input_shape
         assert self.input_size == 15000
         super(AlternateLayer, self).__init__()
-        self.timdistLSTM = nn.LSTM(500, 1, 64, batch_first=True)
-        self.attFCN = nn.Linear(self.seq_len * 30, self.seq_len * 30)
-        self.attRelu = nn.ReLU()
-        self.seqLSTM = nn.LSTM(30, 30, 64, batch_first=True)
+        self.timdistLSTM = nn.LSTM(500, 1, 1, batch_first=True)
+        self.attFCN = nn.Linear(self.seq_len * 30, 30)
+        self.seqLSTM = nn.LSTM(self.seq_len, self.seq_len, batch_first=True)
         self.findense = nn.Linear(self.seq_len, 2)
         self.fintanh = nn.Tanh()
         # Applying the Xavier initialization
@@ -21,17 +20,17 @@ class AlternateLayer(nn.Module):
     def forward(self, x):
         x = x.flip(-1)
         batch_size, seq_len, input_dim = x.size()
-        x = x.reshape(batch_size*seq_len, 30, 500)
-        x, _ = self.timdistLSTM(x)
+        x = x.reshape(batch_size*seq_len*30, 1, 500)
+        _, (x, _) = self.timdistLSTM(x)
         x = x.reshape(batch_size, seq_len, 30)
+        x = x.transpose(1, 2)
         att = x.reshape(batch_size, seq_len*30)
         att = self.attFCN(att)
-        att = self.attRelu(att)
-        att = att.reshape(batch_size, seq_len, 30)
+        att = F.softmax(att, dim =-1)
+        att = att.unsqueeze(-1)
         x = x * att
         x, _ = self.seqLSTM(x)
-        x = F.dropout(x, 0.2)
-        x = x.transpose(1, 2)
+        x = F.dropout(x, 0.2, training = self.training)
         x = self.findense(x)
         x = self.fintanh(x)
         x = x.reshape(batch_size, 60)
