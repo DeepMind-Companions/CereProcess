055fe9575b007dc830fd23ead4e7e1ce18b64028
ArsalanKhan04 <arsal4an@gmail.com>
2024-08-15 14:50:22 +0500

    Add Parameter for minutes added in pipeline

--- datasets/sc_pipeline.py
+++ datasets/sc_pipeline.py
@@ -1,12 +1,17 @@
 from datasets import pipeline as pl
 from .channels import CHANNELS, PAIRS, NMT_CHANNELS, NMT_PAIRS
 
-def get_scratch_pl():
+def get_scratch_pl(dataset = 'TUH', mins = 1):
     pipeline = pl.Pipeline()
-    pipeline.add(pl.CropData(0, 60))
+    if (dataset == 'TUH'):
+        pipeline.add(pl.CropData(0, mins * 60))
+        pipeline.add(pl.ReduceChannels())
+        pipeline.add(pl.BipolarRef())
+    elif (dataset == 'NMT'):
+        pipeline.add(pl.CropData(60, (1+mins) * 60))
+        pipeline.add(pl.ReduceChannels(channels= NMT_CHANNELS))
+        pipeline.add(pl.BipolarRef(pairs=NMT_PAIRS, channels= NMT_CHANNELS))
     pipeline.add(pl.ResampleData(100))
-    pipeline.add(pl.ReduceChannels())
-    pipeline.add(pl.BipolarRef())
     pipeline.add(pl.ClipAbsData(100))
     pipeline.add(pl.HighPassFilter(1.0))
     pipeline.add(pl.NotchFilter(60))
