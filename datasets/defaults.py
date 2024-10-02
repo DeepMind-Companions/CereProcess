f272266804f178969e55346710200460e60bd823
Deep Learning Lab - NCAI <64573724+dll-ncai@users.noreply.github.com>
2024-10-02 16:03:43 +0500

    NMT_NEW dataset added

--- datasets/defaults.py
+++ datasets/defaults.py
@@ -1,12 +1,14 @@
 from datasets.sc_pipeline import get_scratch_pl
+from datasets.sc_pipeline import get_scratch_updated_pl
+
 import numpy as np
 
-tuh_dataset = r'/home/system1/Arsalan/datasets_raw/TUH EEG Corpus'
-nmt_dataset = r'/home/system1/Arsalan/datasets_raw/[M] nmt_scalp_eeg_dataset'
-
+tuh_dataset = r'/home/dll-1/Desktop/eeg/datasets/nmt_scalp_eeg_dataset/'
+nmt_dataset = r'/home/dll-1/Desktop/eeg/datasets/nmt_scalp_eeg_dataset/'
+nmt_new_dataset = r'/media/dll-1/SSD 4TB/EEG Datasets/nmt_v2_scalp_eeg_dataset/'
 
 def get_def_ds(mins = 1):
-    return (tuh_dataset, get_scratch_pl('TUH', mins), np.array([1, 1])), \
-            (nmt_dataset, get_scratch_pl('NMT', mins), np.array([1900, 305]))
-
+    return (tuh_dataset, get_scratch_pl('TUH', mins), np.array([1, 1]), "/media/dll-1/SSD 4TB/EEG Datasets/athar arsalan deepmind/tuh_processed_results_deepmind/"), \
+            (nmt_dataset, get_scratch_updated_pl('NMT', mins), np.array([1900, 305]), "/media/dll-1/SSD 4TB/EEG Datasets/athar arsalan deepmind/nmt_processed_results_deepmind/"), \
+            (nmt_new_dataset, get_scratch_updated_pl('NMT', mins), np.array([3945, 708]), "/media/dll-1/SSD 4TB/EEG Datasets/athar arsalan deepmind/nmt_v2_processed_results_deepmind/")
 
