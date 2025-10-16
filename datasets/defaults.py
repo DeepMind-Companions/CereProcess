8db8cde1a21f5d05e354e2138ae730bb9d1cc560
AtharRizwan <atharrizwan234@gmail.com>
2025-10-16 21:36:40 +0500

    Minor fix

--- datasets/defaults.py
+++ datasets/defaults.py
@@ -10,4 +10,5 @@ nmt_new_dataset = r'/media/tukl/ee279b7d-bb8a-4a20-8bf9-90b2c542efcc/EEG Dataset
 def get_def_ds(mins = 1):
     return (tuh_dataset, get_scratch_pl('TUH', mins), np.array([1, 1]), "results/tuh"), \
             (nmt_dataset, get_scratch_updated_pl('NMT', mins), np.array([1900, 305]), "results/nmt"), \
-            (nmt_new_dataset, get_scratch_updated_pl('NMT', mins), np.array([3945, 708]), "results/nmt4k"), \
+            (nmt_new_dataset, get_scratch_updated_pl('NMT', mins), np.array([3945, 708]), "results/nmt4k")
+
